bd13ccf96a34d7a0456adc4ed90ec2fdb03655c4
VVonneX <78104695+VVonneX@users.noreply.github.com>
2022-06-26 13:04:40 +0300

    Update karma.conf.js

--- web-front/web-frontend/karma.conf.js
+++ web-front/web-frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/web-front/web-frontend'),
+      dir: require('path').join(__dirname, './coverage/web-front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
