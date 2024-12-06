aad4575b7f24cbececc629d31afea7bdf3bb3f7b
Mikhail Churikov <churikov-20@yandex.ru>
2024-12-06 17:04:00 +0300

    develop1: refactor test package and application.properties

--- web-back/src/main/java/ru/churikov/rest/configurations/AuthConfig.java
+++ web-back/src/main/java/ru/churikov/rest/configurations/AuthConfig.java
@@ -1,10 +1,8 @@
 package ru.churikov.rest.configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,7 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -23,7 +20,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 import java.io.IOException;
 
 @Configuration
@@ -44,6 +40,7 @@ public class AuthConfig extends WebSecurityConfigurerAdapter implements WebMvcCo
             throws Exception {
         auth.userDetailsService(UserDetailsService);
     }
+
     // интегрирует UserDetailsService в механизм ауинтификации spring
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
