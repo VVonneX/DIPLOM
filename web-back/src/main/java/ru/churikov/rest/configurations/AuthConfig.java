2aee81db72017b69e2636140699b90314c3f5520
Mikhail Churikov <churikov-20@yandex.ru>
2025-01-10 14:51:53 +0300

    develop: refactor authconfig

--- web-back/src/main/java/ru/churikov/rest/configurations/AuthConfig.java
+++ web-back/src/main/java/ru/churikov/rest/configurations/AuthConfig.java
@@ -3,14 +3,17 @@ package ru.churikov.rest.configurations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -24,32 +27,23 @@ import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
-public class AuthConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+public class AuthConfig implements WebMvcConfigurer {
 
     @Autowired
-    private UserDetailsService UserDetailsService;
-    //Сервис, который отвечает за информацию о пользователе
+    private UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    //Штуки для хеширования паролей в бд, для обеспечения безопасности(чтобы не украли)
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.userDetailsService(UserDetailsService);
-    }
-
-    // интегрирует UserDetailsService в механизм ауинтификации spring
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        System.setProperty("https.protocols", "TLSv1.2");
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         CharacterEncodingFilter filter = new CharacterEncodingFilter();
         filter.setEncoding("UTF-8");
         filter.setForceEncoding(true);
         http.addFilterBefore(filter, CsrfFilter.class);
+
         http
                 .httpBasic().and()
                 .authorizeRequests()
@@ -60,7 +54,7 @@ public class AuthConfig extends WebSecurityConfigurerAdapter implements WebMvcCo
                 .loginProcessingUrl("/auth/login")
                 .and()
                 .logout()
-                .logoutUrl("/auth/logout") // все, что представленно выше, доступных апишек(по сути, настраиваю уровни доступа для пользователей)
+                .logoutUrl("/auth/logout")
                 .logoutSuccessHandler(new LogoutSuccessHandler() {
                     @Override
                     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -69,12 +63,11 @@ public class AuthConfig extends WebSecurityConfigurerAdapter implements WebMvcCo
                                 request.getSession().invalidate();
                             } catch (Exception e) {
                                 e.printStackTrace();
-                                e = null;
                             }
                         }
                         response.setStatus(HttpServletResponse.SC_OK);
                     }
-                }) // она определяет последоватеельноть действий вы успешном выходе пользователя с учетной записи
+                })
                 .deleteCookies("JSESSIONID")
                 .invalidateHttpSession(false)
                 .permitAll()
@@ -82,8 +75,17 @@ public class AuthConfig extends WebSecurityConfigurerAdapter implements WebMvcCo
                 .exceptionHandling()
                 .and()
                 .csrf().disable();
+
+        return http.build();
     }
 
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -91,5 +93,4 @@ public class AuthConfig extends WebSecurityConfigurerAdapter implements WebMvcCo
                 .allowedOrigins("*")
                 .allowedMethods("*");
     }
-    // настройка позволяющая принмать запросы с внешних источников(по сути, общение, условно, между серверами)
 }
